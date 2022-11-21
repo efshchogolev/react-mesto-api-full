11a2403a430b1f3deb01af657d9bbcc135b6d730
Efim Shchegolev <efimblashck@yandex.ru>
2022-11-22 00:01:26 +0300

    feat: исправить cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,28 +8,25 @@ const allowedCors = [
   'https://api.shchegolef.nomoredomains.club/',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
 // eslint-disable-next-line consistent-return
 module.exports = ((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   const requestHeaders = req.headers['access-control-request-headers'];
-
-  res.header('Access-Control-Allow-Credentials', true);
-  // проверяем, что источник запроса есть среди разрешённых
 
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
-  next();
+  return next();
 });
