c814bad0a2cd536d6bd94c878003e6c14759a750
Efim Shchegolev <efimblashck@yandex.ru>
2022-11-21 20:37:39 +0300

    feat: добавить env

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -7,6 +8,8 @@ const {
 const NotFoundError = require('../utils/errors/notFoundError');
 const DataError = require('../utils/errors/dataError');
 const ConflictError = require('../utils/errors/conflictError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -64,7 +67,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   // eslint-disable-next-line consistent-return
   User.checkUserAuth(email, password).then((user) => {
-    const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+    const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
     res.cookie('jwt', token, {
       maxAge: 3600000 * 24 * 7,
       httpOnly: true,
